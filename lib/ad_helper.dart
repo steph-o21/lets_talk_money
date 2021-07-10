ecb7acfdaf25b7880e7628a7515b984eddfdd60d
steph-o21 <77403985+steph-o21@users.noreply.github.com>
2021-07-10 03:48:00 -0400

    Update ad_helper.dart

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -4,9 +4,9 @@ class AdHelper {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3941005348476564/2327741015';
+      return 'ca-app-pub-3941005348476564/2478950108';
     } else if (Platform.isIOS) {
-      return '<ca-app-pub-3941005348476564~7196924317>';
+      return 'ca-app-pub-3941005348476564/4570760977';
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
@@ -14,9 +14,9 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3941005348476564/2327741015';
+      return 'ca-app-pub-3941005348476564/5688880455';
     } else if (Platform.isIOS) {
-      return '<ca-app-pub-3941005348476564~7196924317>';
+      return 'ca-app-pub-3941005348476564/6372650008';
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
@@ -26,7 +26,7 @@ class AdHelper {
     if (Platform.isAndroid) {
       return 'ca-app-pub-3941005348476564/2327741015';
     } else if (Platform.isIOS) {
-      return '<ca-app-pub-3941005348476564~7196924317>';
+      return 'ca-app-pub-3941005348476564/5059568337';
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
